4e61f5e5ef59c49f2a79e6587482f73b0d1afeab
Lindsey Maddox <lindseyemaddox@gmail.com>
2015-03-31 09:41:58 -0500

    post-live edits

--- index.php
+++ index.php
@@ -11,17 +11,19 @@
 
 		<p>The counselors at Rooted pride themselves in making their offices comfortable, welcoming, and warm environments. They recognize that if their client feels uncomfortable in their surroundings, they will have more difficulty connecting on a therapeutic level.</p>
 
-		<h3>and expect to build relationship</h3>
+		<h3>...and expect to build relationship</h3>
 
 		<p>The relationship between the counselors at Rooted and their clients is seen as one of the most important parts of therapy. “If we don’t have relationship, we have nothing.”</p>
 
-		<h3>with licensed practitioners</h3>
+		<h3>...with licensed practitioners</h3>
 
 		<p>The counselors at Rooted are Licensed Professional Counselors in the State of Tennessee. The pursuit of licensure is a stringent process that requires all licensees to attain (at least) two years of supervision after a counselor’s three or more years of graduate studies. Therefore, Rooted counselors have attained at least 5 years of post-undergraduate experience in the counseling field.</p>
 
-		<h3>that use empirically supported therapy techniques.</h3>
+		<h3>...that use empirically supported therapy techniques.</h3>
 
 		<p>Rooted counselors aren’t just shooting in the dark. Though they recognize that the relationship is an integral part of the therapeutic relationship, they also know the importance of using therapy techniques that have been researched and proven successful. </p>
+		
+		<h3>...to facilitate lasting change.</h3>
 
 	</article>
 
